e1769d01a41e86fa7d9a04ba527fd943d7756f70
BrotherZhafif <bangz1504@gmail.com>
2024-09-20 01:49:09 +0700

    Adding New Font and Trying Some Image Fetching Logic

--- asset/script.js
+++ asset/script.js
@@ -1,6 +1,11 @@
 function zoom(element) 
-	{
-		console.log("full size")
-    	element.classList.toggle("fullsize");
-		element.classList.toggle("position-absolute");
-	}
+{
+	console.log("full size")
+	element.classList.toggle("fullsize");
+	element.classList.toggle("position-absolute");
+}
+
+function imageloader(element)
+{
+	
+}
