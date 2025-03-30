395d83272fa1de7f5a238dcd4ae4b1602a472206
brotherzhafif <bangz1504@gmail.com>
2025-03-30 20:54:39 +0700

    Initiating New Portofolio

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
+});
